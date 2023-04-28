33b9388723d55351e0db2b3b5c510c60c95effe6
gzavlanis <gzavlanis@gmail.com>
2023-04-28 18:31:06 +0300

    changes FE

--- ChatGPT-Test/ChatGPT_Test.py
+++ ChatGPT-Test/ChatGPT_Test.py
@@ -7,8 +7,15 @@ from PIL import Image
 # image
 image = Image.open('nn.png')
 
-# model angine AND openai api key
+# model angine
 model_engine = "text-davinci-003"
+
+style = """
+    <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+    </style>
+"""
 
 # function to handle the translation
 def translate_text_simple(text, target_language):
@@ -29,19 +36,23 @@ def main():
     st.sidebar.header('Language Translation App')
     st.sidebar.write('A simple translation app that uses chatGPT api to translate text')
     st.sidebar.write('[About GPT Models](https://en.wikipedia.org/wiki/Generative_pre-trained_transformer)')
-    openai.api_key = st.sidebar.text_input('Enter your OpenAI API key', key = "str")
+    openai.api_key = st.sidebar.text_input('Enter your OpenAI API key', key = "str") # user must enter openAI API key
+    if st.sidebar.button("Refresh page"):
+        st.cache_resource.clear()
     st.sidebar.write('Powered by [OpenAI](https://openai.com)')
     st.image(image)
     text_input = st.text_input('Enter the text you want to translate')
     target_language = st.selectbox('Select language of translation', ['Greek', 'English', 'French', 'Spanish', 'German'])
-    text_type = st.selectbox('Specify the kind of text you want to translate', ['', 'Poem', 'Song', 'Medical document', 'Financial report', 'Proverb'])
+    text_type = st.selectbox('Specify the kind of text you want to translate', ['', 'Poem', 'Song', 'Medical document', 'Financial report', 'Proverb', 'Athletic Team', 'Sport Competition'])
     translate_button = st.button('Translate text')
     translated_text = st.empty()
+    st.markdown(style, unsafe_allow_html = True)
     if translate_button:
         translated_text.text('Translating...')
         if text_type == '':
             translated_text.text(translate_text_simple(text_input, target_language))
         else: translated_text.text(translate_text_specific(text_input, target_language, text_type))
+
 if __name__ == '__main__':
     main()
 
