4f7363b4f9f9e2a011074ba3c98a34687907307c
gzavlanis <gzavlanis@gmail.com>
2023-04-29 13:52:36 +0300

    fe changes

--- ChatGPT-Test/app.py
+++ ChatGPT-Test/app.py
@@ -33,6 +33,7 @@ def translate_text_specific(text, target_language, text_type):
 def main():
 
     # building the frontend
+    st.sidebar.image(image)
     st.sidebar.header('Language Translation App')
     st.sidebar.write('A simple translation app that uses chatGPT api to translate text')
     st.sidebar.write('[About GPT Models](https://en.wikipedia.org/wiki/Generative_pre-trained_transformer)')
@@ -40,7 +41,7 @@ def main():
     if st.sidebar.button("Refresh page"):
         st.cache_resource.clear()
     st.sidebar.write('Powered by [OpenAI](https://openai.com) and [Streamlit](https://streamlit.io)')
-    st.image(image)
+    
     text_input = st.text_input('Enter the text you want to translate')
     target_language = st.selectbox('Select language of translation', ['Greek', 'English', 'French', 'Spanish', 'German'])
     text_type = st.selectbox('Specify the kind of text you want to translate', ['', 'Poem', 'Song', 'Medical document', 'Financial report', 'Proverb', 'Athletic Team', 'Sport Competition'])
